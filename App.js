374cd437a9a767b6bcfd38a34ca6d7f59e14c491
Ayesha Syeda <72366101+asyeda@users.noreply.github.com>
2021-09-19 03:18:07 -0400

    started on SignIn.js

--- App.js
+++ App.js
@@ -3,69 +3,8 @@ import { Text, View, TextInput, TouchableOpacity, Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
-import styles from "./components/globalstyles";
+import SignIn from "./components/Screens/SignIn";
 
-function LoginScreen() {
-  return (
-    <View style={styles.container}>
-
-
-
-      <View style = {styles.logoContainer}>
-        <Image style ={styles.logoPic} source={require('./components/Images/logo.png')}/>
-      </View>
-
-
-
-      {/*<Text style={styles.logo}>HenTat</Text>*/}
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.inputText}
-          placeholder="Email..."
-          placeholderTextColor="#003f5c"
-          //onChangeText={setEmail}
-        />
-      </View>
-      <View style={styles.errMsgBody}>
-        <Text style={styles.errMsg}>{/*emailError*/}</Text>
-      </View>
-      <View style={styles.inputView}>
-        <TextInput
-          secureTextEntry
-          style={styles.inputText}
-          placeholder="Password..."
-          placeholderTextColor="#003f5c"
-          //onChangeText={setPassword}
-        />
-      </View>
-      <View style={styles.errMsgBody}>
-        <Text style={styles.errMsg}>{/*passwordError*/}</Text>
-      </View>
-      <View style={styles.loginView}>
-        <TouchableOpacity style={styles.loginBtn} /*onPress={handleLogin}*/>
-          <Text style={styles.loginText}>LOGIN</Text>
-        </TouchableOpacity>
-      </View>
-      <View>
-        <Text>Don't have an account?</Text>
-      </View>
-      <View style={styles.switchToLoginOrRegister}>
-        <TouchableOpacity>
-          <Text style={styles.signUpText} /*onPress={hasAccountHandler}*/>
-            Sign Up
-          </Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.forgotBody}>
-        <TouchableOpacity>
-          <Text style={styles.forgot} /*onPress={forgotPasswordHandler} */>
-            Forgot Password?
-          </Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
-}
 
 function CameraScreen() {
   return (
@@ -88,37 +27,11 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
   var loggedIn = false;
-  if (loggedIn) {
+  
     return (
-      <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
-
-              if (route.name === "Home") {
-                iconName = focused ? "home" : "home-outline";
-              } else if (route.name === "Camera") {
-                iconName = focused ? "camera" : "camera-outline";
-              }
-
-              // You can return any component that you like here!
-              return <Ionicons name={iconName} size={size} color={color} />;
-            },
-            tabBarActiveTintColor: "tomato",
-            tabBarInactiveTintColor: "gray",
-          })}
-        >
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="Camera" component={CameraScreen} />
-        </Tab.Navigator>
-      </NavigationContainer>
+      <View>
+        <Text>Hi</Text>
+      </View>
+      
     );
-  }
-
-  else {
-    return (
-      <LoginScreen/>
-    );
-  }
 }
---
+++ components/Screens/SignIn.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { Text, View, TextInput, TouchableOpacity, Image } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import styles from "../globalstyles";
+
+export default function SignIn(){
+    return (
+        <View style={{backgroundColor: 'black'}}>
+            <Text>Hi</Text>
+        </View>
+    )
+
+}
