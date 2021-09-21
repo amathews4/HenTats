afd6b28fc4aaa1b4034c15e0758f8620e1ad8a9f
Ayesha Syeda <72366101+asyeda@users.noreply.github.com>
2021-09-21 01:12:18 -0400

    Updated SignUp.js

--- components/Screens/SignUp.js
+++ components/Screens/SignUp.js
@@ -1,5 +1,4 @@
-import React from "react";
-
+import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import {
   Text,
@@ -12,10 +11,41 @@ import {
   Keyboard,
   ImageBackground,
 } from "react-native";
-
 import styles from "../globalstyles";
+import fire from "../Firebase";
 
 export default function SignUp(props) {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [reEnterPassword, setReEnterPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const handleSignup = () => {
+    if (password === reEnterPassword) {
+      fire
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .catch((err) => {
+          switch (err.code) {
+            case "auth/email-already-in-use":
+            case "auth/invalid-email":
+              setEmailError(err.message);
+              break;
+            case "auth/weak-password":
+              setPasswordError(err.message);
+              break;
+          }
+        });
+      fire
+        .database()
+        .ref("users/")
+        .set({
+          email: email.replace(".", ","),
+          fullName: fullName,
+        });
+    } else setPasswordError("Your passwords don't match. Please try again!");
+  };
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
@@ -43,11 +73,10 @@ export default function SignUp(props) {
             <View style={styles.signIn}>
               <View style={styles.inputView}>
                 <TextInput
-                  secureTextEntry
                   style={styles.inputText}
                   placeholder="Name"
                   placeholderTextColor="#00f3c5"
-                  //onChangeText={setName}
+                  onChangeText={setFullName}
                 />
               </View>
               <View style={styles.inputView}>
@@ -55,7 +84,7 @@ export default function SignUp(props) {
                   style={styles.inputText}
                   placeholder="Email"
                   placeholderTextColor="#00f3c5"
-                  //onChangeText={setEmail}
+                  onChangeText={setEmail}
                 />
               </View>
               <View style={styles.inputView}>
@@ -64,7 +93,7 @@ export default function SignUp(props) {
                   style={styles.inputText}
                   placeholder="Password"
                   placeholderTextColor="#00f3c5"
-                  //onChangeText={setPassword}
+                  onChangeText={setPassword}
                 />
               </View>
               <View style={styles.inputView}>
@@ -73,13 +102,14 @@ export default function SignUp(props) {
                   style={styles.inputText}
                   placeholder="Re Enter Password"
                   placeholderTextColor="#00f3c5"
-                  //onChangeText={setReEnterPassword}
+                  onChangeText={setReEnterPassword}
                 />
               </View>
             </View>
             <View style={styles.buttons}>
               <TouchableOpacity
-                style={styles.signUpButton} /*onPress={handleLogin}*/
+                style={styles.signUpButton}
+                onPress={handleSignup}
               >
                 <Text style={styles.loginText}>Get Started</Text>
               </TouchableOpacity>
