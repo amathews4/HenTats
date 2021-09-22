05e480a90b022e5c334da325056208398548789a
Mohfez Rahman <mohfezrahman@gmail.com>
2021-09-21 20:20:56 -0400

    updated

--- components/Screens/Settings.js
+++ components/Screens/Settings.js
@@ -3,24 +3,29 @@ import { Text, View, TextInput, TouchableOpacity, Image } from "react-native";
 import fire from "../Firebase";
 //import { AntDesign } from '@expo/vector-icons';
 //import { SimpleLineIcons } from '@expo/vector-icons';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const handleLogout = () => {
-    fire.auth().signOut();
-}
+  fire.auth().signOut();
+};
 
 export default function Settings() {
-    
-    return(
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" , backgroundColor: "grey"}}>
-  
-            <TouchableOpacity onPress = {handleLogout}>
-            {/* <Text>Log Out</Text>
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "grey",
+      }}
+    >
+      <TouchableOpacity onPress={handleLogout}>
+        {/* <Text>Log Out</Text>
             <AntDesign name="logout" size={24} color="cyan" />
                 */}
-            <MaterialCommunityIcons name="logout" size={24} color="black" />
-            </TouchableOpacity>
-            <Text>Log Out</Text>
-        </View>
-    )
+        <MaterialCommunityIcons name="logout" size={24} color="black" />
+      </TouchableOpacity>
+      <Text>Log Out</Text>
+    </View>
+  );
 }
