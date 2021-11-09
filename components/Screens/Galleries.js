12cda1479c035bde4f2f67484cd626b69b798f65
Ayesha Syeda <72366101+asyeda@users.noreply.github.com>
2021-11-08 22:52:01 -0500

    added new tattoos/henna designs

--- components/Screens/Galleries.js
+++ components/Screens/Galleries.js
@@ -16,15 +16,21 @@ export default class Galleries extends Component {
     this.state = {
       data: [
         {id:1,  image:require("../Images/1.png")},
-        {id:2,  image:require("../Images/2.jpg")} ,
+        {id:2,  image:require("../Images/2.png")} ,
         {id:3,  image:require("../Images/3.png")}, 
-        {id:4,  image:require("../Images/4.jpg")}, 
+        {id:4,  image:require("../Images/4.png")}, 
         {id:5,  image:require("../Images/5.png")}, 
         {id:6,  image:require("../Images/6.png")}, 
         {id:7,  image:require("../Images/7.png")}, 
-        {id:8,  image:require("../Images/8.jpg")},
-        {id:9,  image:require("../Images/9.jpg")},
-        {id:10, image:require("../Images/10.jpg")},
+        {id:8,  image:require("../Images/8.png")},
+        {id:9,  image:require("../Images/9.png")},
+        {id:10, image:require("../Images/10.png")},
+        {id:11, image:require("../Images/11.png")},
+        {id:12, image:require("../Images/12.png")},
+        {id:13, image:require("../Images/13.png")},
+        {id:14, image:require("../Images/14.png")},
+        {id:15, image:require("../Images/15.png")},
+        {id:16, image:require("../Images/16.png")},
       ]
     };
   }
@@ -50,7 +56,6 @@ export default class Galleries extends Component {
             const item = post.item.image;
             return (
               <View style={styles.card}>
-                {console.log(item)}
                 <TouchableOpacity onPress = {() => this.imageHandler(item)}> 
                   <Image style={styles.cardImage} source={item}/>
                 </TouchableOpacity>
